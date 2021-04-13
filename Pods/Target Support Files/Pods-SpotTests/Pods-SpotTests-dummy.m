2054d0812674b4015f3878ac13d8287963970716
Kenny Barone <kennybarone@gmail.com>
2021-04-13 17:46:01 -0400

    0.9 (13)

--- Pods/Target Support Files/Pods-SpotTests/Pods-SpotTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SpotTests : NSObject
-@end
-@implementation PodsDummy_Pods_SpotTests
-@end
--- Pods/Target Support Files/Pods-SpotUITests/Pods-SpotUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SpotUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SpotUITests
-@end
