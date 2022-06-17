a64a9ec7b94fda8bc354b4ad52e3e66ce4033c74
Arnold Lee <arnold.kn.lee@gmail.com>
2022-06-17 15:35:13 -0400

    UISheetPresentationController: Disable drag to dismiss UISheetPresentationController: When in medium detent remove the shrink animation from the view below UISheetPresentationController: Rearrange detent order to comply with Apply guidelines BottomToTopTransition: Change transition animation duration DrawerView: Now can set position to present first DrawerView: Change wordings DrawerView: Change grabber width DrawerView: Refine panPerforming code

--- Spot/Views/SupplementalViews/DrawerView.swift
+++ Spot/Views/SupplementalViews/DrawerView.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SnapKit
 
-enum Status {
+enum DrawerViewStatus {
     case Top
     case Middle
     case Bottom
@@ -17,7 +17,6 @@ enum Status {
 }
 
 class DrawerView: NSObject {
-        
     private lazy var slideView = UIView {
         $0.backgroundColor = .clear
         $0.layer.cornerRadius = 10
@@ -26,13 +25,11 @@ class DrawerView: NSObject {
         $0.layer.shadowOpacity = 0.8
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
-    
     private lazy var closeButton = UIButton {
         $0.backgroundColor = .clear
         $0.setImage(UIImage(systemName: "xmark.circle.fill", withConfiguration: UIImage.SymbolConfiguration(textStyle: .largeTitle))?.withTintColor(.tertiarySystemFill, renderingMode: .alwaysOriginal), for: .normal)
         $0.setTitle("", for: .normal)
     }
-    
     private lazy var grabberView = UIView {
         $0.backgroundColor = .tertiarySystemFill
         $0.layer.cornerRadius = 2
@@ -40,7 +37,13 @@ class DrawerView: NSObject {
     
     private var rootVC = UIViewController()
     private unowned var parentVC: UIViewController = UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController ?? UIViewController()
-    private var status = Status.Close
+    
+    private var status = DrawerViewStatus.Close
+    private var duration: CGFloat = 0
+    private var yPosition: CGFloat = 0
+    private var topConstraints: Constraint? = nil
+    private var midConstraints: Constraint? = nil
+    private var botConstraints: Constraint? = nil
     
     override init() {
         super.init()
@@ -61,12 +64,17 @@ class DrawerView: NSObject {
         parentVC.view.addSubview(slideView)
         slideView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
-            $0.top.greaterThanOrEqualTo(parentVC.view.snp.top).offset(0.45 * parentVC.view.frame.height)
+            topConstraints = $0.top.greaterThanOrEqualTo(parentVC.view.snp.top).offset(100).constraint
+            midConstraints = $0.top.greaterThanOrEqualTo(parentVC.view.snp.top).offset(0.45 * parentVC.view.frame.height).constraint
+            botConstraints = $0.top.greaterThanOrEqualTo(parentVC.view.snp.bottom).inset(100).constraint
             $0.height.equalTo(parentVC.view.snp.height)
         }
-        slideView.frame = CGRect(x: 0, y: parentVC.view.frame.height - 100, width: parentVC.view.frame.width, height: parentVC.view.frame.height)
+        topConstraints?.deactivate()
+        midConstraints?.deactivate()
+        botConstraints?.deactivate()
+        slideView.frame = CGRect(x: 0, y: parentVC.view.frame.height, width: parentVC.view.frame.width, height: parentVC.view.frame.height)
+        
         slideView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.panPerforming(recognizer:))))
-       
         let myNav = UINavigationController(rootViewController: rootVC)
         parentVC.addChild(myNav)
         slideView.addSubview(myNav.view)
@@ -74,21 +82,64 @@ class DrawerView: NSObject {
         myNav.view.layer.cornerRadius = cornerRadius
         myNav.view.layer.masksToBounds = true
         myNav.didMove(toParent: parentVC)
-        
         slideView.addSubview(grabberView)
         grabberView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(10)
-            $0.width.equalTo(50)
+            $0.width.equalTo(40)
             $0.height.equalTo(4)
             $0.centerX.equalToSuperview()
         }
-        
         slideView.addSubview(closeButton)
         closeButton.snp.makeConstraints {
             $0.top.trailing.equalToSuperview()
             $0.width.height.equalTo(70)
         }
         closeButton.addTarget(self, action: #selector(self.closeAction), for: .touchUpInside)
+    }
+    
+    public func present(to: DrawerViewStatus = .Middle) {
+        let currentStatus = status
+        switch to {
+        case .Top:
+            goTop()
+        case .Middle, .Close:
+            goMid()
+        case .Bottom:
+            goBot()
+        }
+        if currentStatus != to {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                self.slideView.frame.origin.y = self.yPosition
+            }, completion: nil)
+        } else {
+            let animation = CAKeyframeAnimation(keyPath: "transform.translation.y")
+            animation.values = [0, -20, 0]
+            animation.duration = 0.5
+            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+            slideView.layer.add(animation, forKey: nil)
+        }
+    }
+    
+    private func goTop() {
+        topConstraints?.activate()
+        let sheetPresentVelocity = 0.35 * self.parentVC.view.frame.height / 0.5
+        duration = abs((self.parentVC.view.frame.height - self.slideView.frame.height + 100 - self.slideView.frame.origin.y) / sheetPresentVelocity)
+        yPosition = (self.parentVC.view.frame.height - self.slideView.frame.height + 100)
+        self.status = DrawerViewStatus.Top
+    }
+    private func goMid() {
+        midConstraints?.activate()
+        let sheetPresentVelocity = 0.35 * self.parentVC.view.frame.height / 0.5
+        duration = abs((0.45 * self.parentVC.view.frame.height - self.slideView.frame.origin.y) / sheetPresentVelocity)
+        yPosition = (0.45 * self.parentVC.view.frame.height)
+        self.status = DrawerViewStatus.Middle
+    }
+    private func goBot() {
+        botConstraints?.activate()
+        let sheetPresentVelocity = 0.35 * self.parentVC.view.frame.height / 0.5
+        duration = abs((self.parentVC.view.frame.height - 100 - self.slideView.frame.origin.y) / sheetPresentVelocity)
+        yPosition = self.parentVC.view.frame.height - 100
+        self.status = DrawerViewStatus.Bottom
     }
     
     // Pan gesture
@@ -102,26 +153,6 @@ class DrawerView: NSObject {
             recognizer.setTranslation(.zero, in: recognizer.view)
         }
         else{
-            let sheetPresentVelocity = 0.35 * self.parentVC.view.frame.height / 0.5
-            var duration: CGFloat = 0
-            var yPosition: CGFloat = 0
-            
-            func goTop() {
-                duration = (self.parentVC.view.frame.height - self.slideView.frame.height + 100 - self.slideView.frame.origin.y) / sheetPresentVelocity
-                yPosition = (self.parentVC.view.frame.height - self.slideView.frame.height + 100)
-                self.status = Status.Top
-            }
-            func goMid() {
-                duration = (0.45 * self.parentVC.view.frame.height - self.slideView.frame.origin.y) / sheetPresentVelocity
-                yPosition = (0.45 * self.parentVC.view.frame.height)
-                self.status = Status.Middle
-            }
-            func goBot() {
-                duration = (self.parentVC.view.frame.height - 100 - self.slideView.frame.origin.y) / sheetPresentVelocity
-                yPosition = self.parentVC.view.frame.height - 100
-                self.status = Status.Bottom
-            }
-            
             // Check the velocity of gesture to determine if it's a swipe or a drag
             if abs(recognizer.velocity(in: recognizer.view).y) > 1000 {
                 // Swipe up velocity is smaller than 0
@@ -145,7 +176,7 @@ class DrawerView: NSObject {
                 }
             }
             UIView.animate(withDuration: duration) {
-                self.slideView.frame.origin.y = yPosition
+                self.slideView.frame.origin.y = self.yPosition
                 self.parentVC.view.layoutIfNeeded()
             }
         }
@@ -156,30 +187,7 @@ class DrawerView: NSObject {
             self.slideView.frame.origin.y = self.parentVC.view.frame.height
             self.parentVC.view.layoutIfNeeded()
         }) { (success) in
-            self.status = Status.Close
-        }
-    }
-    
-    public func present() {
-        switch status {
-        case .Top, .Bottom:
-            UIView.animate(withDuration: 0.35) {
-                self.slideView.frame.origin.y = (0.45 * self.parentVC.view.frame.height)
-            } completion: { success in
-                self.status = Status.Middle
-            }
-        case .Middle:
-            let animation = CAKeyframeAnimation(keyPath: "transform.translation.y")
-            animation.values = [0, -20, 0]
-            animation.duration = 0.5
-            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
-            slideView.layer.add(animation, forKey: nil)
-        case .Close:
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
-                self.slideView.frame.origin.y = 0.45 * self.parentVC.view.frame.height
-            }) { (success) in
-                self.status = Status.Middle
-            }
+            self.status = DrawerViewStatus.Close
         }
     }
     
