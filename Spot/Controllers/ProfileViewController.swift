383eb7af8ffcbe4cbda4c25e277e1c9b89be8253
Arnold Lee <arnold.kn.lee@gmail.com>
2022-06-22 15:40:34 -0400

    DrawerView: Fix unstable transition animation

--- Spot/Controllers/ProfileViewController.swift
+++ Spot/Controllers/ProfileViewController.swift
@@ -8,10 +8,8 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController, UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
-    
-    private let transitionAnimation = BottomToTopTransition()
-    
+class ProfileViewController: UIViewController {
+        
     // function for adding profileViewController
     @objc func addView(_ sender: UIButton){
         let newVC = ProfileViewController()
@@ -20,37 +18,12 @@ class ProfileViewController: UIViewController, UIViewControllerTransitioningDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
         modalPresentationStyle = .custom
-        navigationController?.delegate = self
-        
         let myButton = UIButton(type: .system)///dummy button for adding profile view
         myButton.frame = CGRect(x: 20, y: 130, width: 100, height: 50)
         myButton.setTitle("AddView", for: .normal)
         myButton.addTarget(self, action: #selector(addView(_:)), for: .touchUpInside)
         view.addSubview(myButton)
-        self.view = view
-    }
-    
-    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transitionAnimation.transitionMode = operation == .push ? .present : .pop
-        return transitionAnimation
-    }
-    
-    override func viewWillAppear(_ animated: Bool){
-        super.viewWillAppear(animated)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.becomeFirstResponder()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool){
-        super.viewWillDisappear(animated)
-    }
-    
-    override func viewDidDisappear(_ animated: Bool){
-        super.viewDidDisappear(animated)
     }
 }
