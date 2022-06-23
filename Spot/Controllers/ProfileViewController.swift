51ca229e077474a47d96d937d4e6aec24d6f6e62
Arnold <arnold.kn.lee@gmail.com>
2022-06-23 10:49:35 -0400

    Initial Commit

--- Spot/Controllers/ProfileViewController.swift
+++ Spot/Controllers/ProfileViewController.swift
@@ -7,23 +7,10 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileViewController: UIViewController {
-        
-    // function for adding profileViewController
-    @objc func addView(_ sender: UIButton){
-        let newVC = ProfileViewController()
-        navigationController?.pushViewController(newVC, animated: true)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
-        modalPresentationStyle = .custom
-        let myButton = UIButton(type: .system)///dummy button for adding profile view
-        myButton.frame = CGRect(x: 20, y: 130, width: 100, height: 50)
-        myButton.setTitle("AddView", for: .normal)
-        myButton.addTarget(self, action: #selector(addView(_:)), for: .touchUpInside)
-        view.addSubview(myButton)
     }
 }
