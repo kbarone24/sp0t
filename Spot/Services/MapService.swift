f32403c56edcdc9f2a0647cfc5ad86c1a6311af6
Kanjiz1 <kanjis007@gmail.com>
2022-10-21 23:33:51 -0500

    Added service container, added maps service, refactoring...

---
+++ Spot/Services/MapService.swift
@@ -0,0 +1,27 @@
+//
+//  MapService.swift
+//  Spot
+//
+//  Created by Oforkanji Odekpe on 10/21/22.
+//  Copyright © 2022 sp0t, LLC. All rights reserved.
+//
+
+import Firebase
+import Foundation
+
+// This is what will be used for app map API calls going forward
+
+protocol MapServiceProtocol {
+    func fetchMaps()
+    func fetchMapPosts()
+}
+
+final class MapService: MapServiceProtocol {
+    func fetchMaps() {
+
+    }
+
+    func fetchMapPosts() {
+
+    }
+}
