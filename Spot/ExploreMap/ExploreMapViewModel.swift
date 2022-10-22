a75d2dfebacba79a2420c37e0c898caa6748014f
Kanjiz1 <kanjis007@gmail.com>
2022-10-22 02:46:26 -0500

    Added explore map view controller and view model.

---
+++ Spot/ExploreMap/ExploreMapViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ExploreMapViewModel.swift
+//  Spot
+//
+//  Created by Oforkanji Odekpe on 10/22/22.
+//  Copyright © 2022 sp0t, LLC. All rights reserved.
+//
+
+import Foundation
+import Combine
+import Firebase
+
+final class ExploreMapViewModel {
+    typealias Section = ExploreMapViewController.Section
+    typealias Item = ExploreMapViewController.Item
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Item>
+    
+    private let service: MapServiceProtocol
+    
+    init(serviceContainer: ServiceContainer) {
+        guard let mapService = try? serviceContainer.service(for: \.mapsService) else {
+            service = MapService(fireStore: Firestore.firestore())
+            return
+        }
+        
+        service = mapService
+    }
+}
